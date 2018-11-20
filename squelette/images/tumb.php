a37af38295be1b969b3930cfc4b3cb53e68d490f
Pho3 <rainbowdashtheone@gmail.com>
2018-11-21 00:13:49 +0100

    thumb gen

--- squelette/images/tumb.php
+++ squelette/images/tumb.php
@@ -1,24 +1,35 @@
 <?php
-function LoadGif($imgname)
+function genBlanc($im) {
+	$im = imagecreatetruecolor (150, 30);
+	$bgc = imagecolorallocate ($im, 255, 255, 255);
+	$tc = imagecolorallocate ($im, 0, 0, 0);
+
+	imagefilledrectangle ($im, 0, 0, 150, 30, $bgc);
+	imagestring ($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
+	return $im;
+}
+
+function LoadGif($img)
 {
-    /* Tente d'ouvrir l'image */
-    $im = @imagecreatefromgif($imgname);
+	$im = @imagecreatefromgif($img);
+	if(!$im) return genBlanc($im);	
+	$sx = imagesx($im);
+	$sy = imagesy($im);
+	$dg = ($sx/$sy)*256;
+	$i = imagecreatetruecolor($dg, 256);
+	imagecopyresized($i, $im, 0, 0, 0, 0, $dg, 256, $sx, $sy);
+	return $im;
+}
 
-    /* Traitement si l'ouverture a échoué */
-    if(!$im)
-    {
-        /* Création d'une image vide */
-        $im = imagecreatetruecolor (150, 30);
-        $bgc = imagecolorallocate ($im, 255, 255, 255);
-        $tc = imagecolorallocate ($im, 0, 0, 0);
-
-        imagefilledrectangle ($im, 0, 0, 150, 30, $bgc);
-
-        /* Affiche un message d'erreur dans l'image */
-        imagestring ($im, 1, 5, 5, 'Error loading ' . $imgname, $tc);
-    }
-
-    return $im;
+function LoadJpg($img) {
+	$im = @imagecreatefromjpeg($img);
+	if(!$im) return genBlanc($im);
+	$sx = imagesx($im);
+	$sy = imagesy($im);
+	$dg = ($sx/$sy)*256;
+	$i = imagecreatetruecolor($dg, 256);
+	imagecopyresized($i, $im, 0, 0, 0, 0, $dg, 256, $sx, $sy);
+	return $i;
 }
 
 $id = '2';
@@ -26,21 +37,29 @@ $name = '5ec6d5605d0c';
 $ext = 'gif';
 //header('Content-Type: image/gif');
 
-if(0) {
+if(1) {
 
-  $files = scandir('../media/');
-  foreach($files as $file) {
-    $p = explode('.', $file);
-    if($p[0]!=null) {
-      echo '../media/'.$p[0].'.'.$p[1].'<br>';
-      $img = LoadGif('../media/'.$p[0].'.'.$p[1]);
-      imagegif($img, '../media/'.$p[0].'_thumb.jpg');
-    }
-  }
+	$files = scandir('../media/');
+	foreach($files as $file) {
+		$p = explode('.', $file);
+		$s = explode('_thumb', $file);
+		if(array_key_exists('1', $s)) $p[0] = null;
+		if($p[0]!=null) {
+			echo '../media/'.$p[0].'.'.$p[1].'<br>';
+			if($p[1]=='gif') {
+				$img = LoadGif('../media/'.$p[0].'.'.$p[1]);
+			       	imagegif($img, '../media/'.$p[0].'_thumb.jpg');
+			}
+			if($p[1]=='jpg') {
+				$img = LoadJpg('../media/'.$p[0].'.'.$p[1]);
+				imagejpeg($img, '../media/'.$p[0].'_thumb.jpg');
+			}
+		}
+		//$img = LoadGif('../media/'.$id.'_'.$name.'.'.$ext);
+
+		//imagegif($img, $id.'_'.$name.'_thumb.jpg');
+		//imagedestroy($img);
+	}
 }
 
-$img = LoadGif('../media/'.$id.'_'.$name.'.'.$ext);
 
-imagegif($img, $id.'_'.$name.'_thumb.jpg');
-//imagedestroy($img);
-?>
