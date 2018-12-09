db1f919b8fbb7971841fa0f04e0664a6643340bb
Pho3 <pho3bechan@gmail.com>
2018-12-09 01:40:56 +0100

    I'm definitly dumb.

---
+++ squelette/Arch/view/usersSuccess.php
@@ -0,0 +1,44 @@
+<?php
+  if($context->logged == false) {
+    die("Please go login");
+  }
+ ?>
+<nav class="col col-lg-3 sidebar dashboard dashboard-left mob-hidden">
+  <div class="BProfile">
+    <div class="card-deck">
+      <div class="card">
+        <?php genProfile($context->user, true) ?>
+    </div>
+  </div>
+  <div class="BSug">
+    <div class="card d-flex col-centered">
+      <div class="card-header">
+        Suggestions
+      </div>
+      <ul class="list-group list-group-flush">
+
+        <?php echo genSuggestions($context->sug);?>
+
+      </ul>
+      <div class="card-body suggestions">
+        <a href="?action=listUsers" class="card-link">View more</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<div class="col posts-main ">
+  <div class="row profiles">
+    <?php
+
+    foreach ($context->users as $user) {
+      ?>
+    <div class="col-lg-4">
+      <div class="card-deck">
+        <div class="card">
+          <?php genProfile($user, false, true) ?>
+        </div>
+      </div>
+    </div>
+    <?php } ?>
+  </div>
+</div>
