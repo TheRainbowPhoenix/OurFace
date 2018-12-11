f044c540260c8af6a02a8f784b9476f62ca1949f
Pho3 <rainbowdashtheone@gmail.com>
2018-12-11 05:59:06 +0100

    still working, with dbconnexion

--- squelette/api/poll.php
+++ squelette/api/poll.php
@@ -7,29 +7,33 @@ require_once '../lib/core.php';
  */
 
 if (empty($_REQUEST['from'])) {
-    $id = 0;
+	$id = 0;
 } else {
-    $id = (int)$_REQUEST['from'];
+	$id = (int)$_REQUEST['from'];
 }
 
+$dbc = new dbconnection();
+
 $db = new PDO('pgsql:host='.HOST.';dbname='.DB, USER, PASS, array(
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ));
 
 function fetch($id = 0, $limit = 10) {
-    global $db;
+	//global $db;
+	global $dbc;
 
-    $stmt = $db->prepare("SELECT * FROM fredouil.post WHERE id > ".$id." ORDER BY id DESC LIMIT ".$limit);
-    $stmt->execute();
+	return $dbc->doQuery("SELECT * FROM fredouil.post WHERE id > ".$id." ORDER BY id DESC LIMIT ".$limit);
+	/*$stmt = $db->prepare("SELECT * FROM fredouil.post WHERE id > ".$id." ORDER BY id DESC LIMIT ".$limit);
+	$stmt->execute();
 
-    return $stmt->fetchAll();
+	return $stmt->fetchAll();*/
 }
 
 function output($val = array()) {
-    header('Content-type: application/json; charset=utf-8');
-    echo json_encode($val);
-    exit;
+	header('Content-type: application/json; charset=utf-8');
+	echo json_encode($val);
+	exit;
 }
 
 set_time_limit(45);
@@ -37,12 +41,12 @@ set_time_limit(45);
 $ret = fetch($id);
 
 if (empty($ret)) {
-    $db->exec('LISTEN post');
-    $r = $db->pgsqlGetNotify(PDO::FETCH_ASSOC, 30000);
-    if ($r === false) {
-        output();
-    }
-    $ret = fetch($id);
+	$db->exec('LISTEN post');
+	$r = $db->pgsqlGetNotify(PDO::FETCH_ASSOC, 30000);
+	if ($r === false) {
+		output();
+	}
+	$ret = fetch($id);
 }
 
 output($ret);
